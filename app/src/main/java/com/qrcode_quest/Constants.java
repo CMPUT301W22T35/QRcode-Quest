b18e28f01e66779481cd3f5b24f2a36e0e8c652b
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-03-12 10:30:11 -0700

    Added constants class

---
+++ app/src/main/java/com/qrcode_quest/Constants.java
@@ -0,0 +1,7 @@
+package com.qrcode_quest;
+
+public class Constants {
+    public final static String SHARED_PREF_PATH = "qrcode_quest";
+    public final static String DEVICE_UID_PREF = "device_uid";
+    public final static String AUTHED_USERNAME_PREF = "authed_player";
+}
