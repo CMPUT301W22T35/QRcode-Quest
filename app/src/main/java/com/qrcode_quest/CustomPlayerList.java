59befbaa4348d4c2f934fb578cabda33a33a65d0
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-03-12 13:39:20 -0700

    Merged agellog

---
+++ app/src/main/java/com/qrcode_quest/CustomPlayerList.java
@@ -0,0 +1,58 @@
+package com.qrcode_quest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.qrcode_quest.entities.PlayerAccount;
+
+import java.util.ArrayList;
+
+public class CustomPlayerList extends ArrayAdapter<PlayerAccount> {
+    private ArrayList<PlayerAccount> players;
+    // private PlayerManager playerManager;
+    // private Boolean isLeaderboardList;
+    private Context context;
+
+    public CustomPlayerList(Context context, ArrayList<PlayerAccount> players) {
+        super(context, 0, players);
+        this.players = players;
+        this.context = context;
+        // this.isLeaderboardList = isLeaderBoardList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+
+        View view = convertView;if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.player_list_content, parent,false);
+        }
+
+        PlayerAccount player = players.get(position);
+
+        TextView playerName = view.findViewById(R.id.player_text);
+        TextView score_text = view.findViewById(R.id.score_text);
+
+        playerName.setText(player.getUsername());
+
+        // Set score for leaderboards...
+        /*playerManager = new PlayerManager();
+        if (isLeaderBoardList) {
+            score_text.setText(playerManager.getScore(player));  ?
+        }
+        else {
+            score_text.setVisibility(View.GONE);
+        }*/
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/qrcode_quest/ui/leaderboard/LeaderboardFragment.java
@@ -0,0 +1,55 @@
+package com.qrcode_quest.ui.leaderboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.qrcode_quest.PlayerListFragment;
+import com.qrcode_quest.R;
+import com.qrcode_quest.databinding.FragmentLeaderboardBinding;
+
+public class LeaderboardFragment extends Fragment {
+    Fragment playerListFragment;
+    // Integer rLayoutId
+    // FirebaseFirestore database;
+
+    private FragmentLeaderboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LeaderboardViewModel leaderboardViewModel =
+                new ViewModelProvider(this).get(LeaderboardViewModel.class);
+
+        binding = FragmentLeaderboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textLeaderboard;
+        leaderboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        String[] players = {"QRHUNTER12", "PLAYER1", "PLAYER2"}; // replace with firebase later
+        playerListFragment = PlayerListFragment.newInstance(players);
+
+        FragmentManager fragmentManager = getChildFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentContainerView2, playerListFragment);
+        fragmentTransaction.commit();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
