bfb7f5ae6025f5df4b62a98760ef053551a4d3f5
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-03-13 09:51:31 -0600

    Fixed login post-merge

--- app/src/main/java/com/qrcode_quest/database/PlayerManager.java
+++ app/src/main/java/com/qrcode_quest/database/PlayerManager.java
@@ -28,6 +28,7 @@ public class PlayerManager extends DatabaseManager {
     public PlayerManager(FirebaseFirestore db) {
         super(db);
     }
+    public PlayerManager() { super(); }
 
     /**
      * Fetches a player's data from the database
