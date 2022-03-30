521f54db3e4700da70dfb40b9a2169a2a21865a1
KarlHanEdn <tianming@ualberta.ca>
2022-03-30 16:18:09 -0600

    update MainActivityTest for permissions since MainActivity now requires permissions on map branch

--- app/src/androidTest/java/com/qrcode_quest/MainActivityTest.java
+++ app/src/androidTest/java/com/qrcode_quest/MainActivityTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
 
+import android.Manifest;
 import android.util.Log;
 import android.view.View;
 
@@ -24,6 +25,7 @@ import androidx.test.espresso.UiController;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
 
 import com.qrcode_quest.application.AppContainer;
 import com.qrcode_quest.application.QRCodeQuestApp;
@@ -37,6 +39,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public GrantPermissionRule grantPermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.INTERNET
+    );
 
     public ActivityScenarioRule<MainActivity> rule;
 
