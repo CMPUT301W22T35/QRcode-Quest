13ca9999f410ee0a620a63dac6ef8b592b386745
jaBuOt <burgosot@ualberta.ca>
2022-03-31 01:11:57 -0600

    added profile/account UI and slight changes to AccountFragment

--- app/src/main/java/com/qrcode_quest/ui/account/AccountFragment.java
+++ app/src/main/java/com/qrcode_quest/ui/account/AccountFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +14,41 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.qrcode_quest.CaptureActivity;
+import com.qrcode_quest.MainViewModel;
 import com.qrcode_quest.databinding.FragmentAccountBinding;
+import com.qrcode_quest.entities.PlayerAccount;
+import com.qrcode_quest.entities.RawQRCode;
+import com.qrcode_quest.ui.playerQR.PlayerQRListFragmentArgs;
 
 public class AccountFragment extends Fragment {
 
+    private MainViewModel mainViewModel;
     private FragmentAccountBinding binding;
+    private PlayerAccount player;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        // Load the player argument
+        player = PlayerQRListFragmentArgs.fromBundle(getArguments()).getPlayer();
+
+        binding.playerUsername.setText(player.getUsername());
+
+        if (player.getEmail() != null) {
+            binding.playerEmail.setText(player.getEmail());
+        }
+        else{
+            binding.playerEmail.setText("Not Provided");
+        }
+
+        binding.qrgenerateProfileButton.setOnClickListener(v->{
+            generateProfileCode();
+        });
+
+        binding.qrgenerateLoginButton.setOnClickListener(v->{
+            generateLoginCode();
+        });
+
         AccountViewModel accountViewModel =
                 new ViewModelProvider(this).get(AccountViewModel.class);
 
@@ -33,17 +61,19 @@ public class AccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(), CaptureActivity.class));
-            }
-        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public RawQRCode generateProfileCode(){
+        return new RawQRCode(player.getUsername() + "Profile");
+    }
+
+    public RawQRCode generateLoginCode(){
+        return new RawQRCode(player.getUsername() + "Login");
     }
 }
